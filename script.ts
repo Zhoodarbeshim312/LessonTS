5aaa68dcd264420f928a554013975f73f8fc561c
Joodarbeshim05 <162398776+Joodarbeshim05@users.noreply.github.com>
2025-08-26 02:16:00 +0600

    first commit

---
+++ script.ts
@@ -0,0 +1,104 @@
+// TypeScript
+
+// let num: number = 5;
+// let str: string = "Hello";
+// let boolean: boolean = true;
+
+// function action(main: number): number {
+//   return main + 12;
+// }
+// action(12);
+
+// object
+
+// interface Iperson {
+//   id: number;
+//   name: string;
+//   job: string;
+//   hobby?: string;
+// }
+
+// let person: Iperson = {
+//   id: 1,
+//   name: "Toni Montano",
+//   job: "Gangster",
+// };
+
+// Array
+
+// let arr: number[] = [1, 2, 3, 4, 5];
+
+// interface Iperson {
+//   id: number;
+//   name: string;
+//   job: string;
+//   killed: IKilled[];
+// }
+
+// interface IUsers {
+//   id: number;
+//   name: string;
+// }
+
+// const users: IUsers[] = [
+//   {
+//     id: 1,
+//     name: "Hulk",
+//   },
+//   {
+//     id: 2,
+//     name: "Tour",
+//   },
+//   {
+//     id: 3,
+//     name: "Spider",
+//   },
+// ];
+
+// interface IKilled {
+//   id: number;
+//   name: string;
+// }
+
+// let person: Iperson = {
+//   id: 1,
+//   name: "Toni Montano",
+//   job: "Gangster",
+//   killed: [
+//     {
+//       id: 1,
+//       name: "Friend",
+//     },
+//   ],
+// };
+
+// practice
+// let arr: (string | number | boolean)[] = [
+//   "JavaScript",
+//   "TypeScript",
+//   12,
+//   23,
+//   true,
+// ];
+
+// interface IUser {
+//   id: number;
+//   name: string;
+//   action: boolean;
+//   about: IAbout[];
+// }
+// interface IAbout {
+//   age: number;
+// }
+// let arr: IUser[] = [
+//   {
+//     id: 1,
+//     name: "Done",
+//     action: true,
+//     about: [
+//       {
+//         age: 10,
+//       },
+//     ],
+//   },
+// ];
